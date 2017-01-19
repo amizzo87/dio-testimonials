80d6c343a73f5da84503b9f386bff1f2b7f3750b
Anthony Izzo <Anthony@Anthonys-MacBook-Pro.local>
2017-01-19 12:17:39 -0800

    adding outputs for all fields and image url

--- dio-testimonials.php
+++ dio-testimonials.php
@@ -28,14 +28,16 @@ $wp->add_query_var('em_domain');
 
 private function export_testimonials()
 {
+global $wpdb;
 $outstring  = '<html>';
 $outstring .= '<head><style>';
 $outstring .= '';
 $outstring .= '</style></head><body>';
 
 /* Here we get recent testimonials for the blog */
+
 $args = array(
-'numberposts' => 5,
+'numberposts' => 100,
 'offset' => 0,
 'category' => 0,
 'orderby' => 'post_date',
@@ -45,12 +47,38 @@ $args = array(
 'suppress_filters' => true
 );
 
+
 $recent_posts = wp_get_recent_posts($args);
 
 $outstring .= '<div class=”widget-testimonials”>';
 foreach($recent_posts as $recent)
 {
-$outstring .= '<div class="testimonial"><p class="testimonial-author">'.$recent["post_title"].'</p><p class="testimonial-content">'.$recent["post_content"].'</p></div><br />';
+$querystr = "
+    SELECT $wpdb->posts.*
+    FROM $wpdb->posts
+    WHERE $wpdb->posts.ID = (SELECT $wpdb->postmeta.meta_value FROM $wpdb->postmeta WHERE $wpdb->postmeta.meta_key = '_thumbnail_id' AND $wpdb->postmeta.post_id = '".$recent["ID"]."') 
+ ";
+$thumbUrl = $wpdb->get_results($querystr, OBJECT);
+$outstring .= '<div class="testimonial">';
+foreach($thumbUrl as $url) {
+setup_postdata($url);
+$outstring .= '<div class="testimonial-logoUrl">'.$url->guid.'</div>';
+}
+
+$metastr = "
+    SELECT $wpdb->postmeta.*
+    FROM $wpdb->postmeta
+    WHERE $wpdb->postmeta.post_id = '".$recent["ID"]."'
+    AND $wpdb->postmeta.meta_key = 'thegem_testimonial_data'
+ ";
+$metas = $wpdb->get_results($metastr, OBJECT);
+foreach($metas as $meta) {
+setup_postdata($meta);
+$outstring .= '<div class="testimonial-rawData">'.$meta->meta_value.'</div>';
+}
+
+$outstring .= '<p class="testimonial-author">'.$recent["post_title"].'</p><p class="testimonial-content">'.$recent["post_content"].'</p></div><br />';
+
 }
 
 $outstring .= '</div>';
